a45c9b7923becf64190289af177ddb83515ffc53
Filip Cyprowski <filip.cyprowski@lingarogroup.com>
2019-12-07 18:57:07 +0100

    wstepna eksploracja - przykłady

--- titanic/run.R
+++ titanic/run.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(caret)
 library(dplyr)
+library(tidyr)
 library(readr)
 
 
@@ -9,3 +10,41 @@ setwd("~/politechnika/titanic")
 getwd()
 
 train_set = read_csv("../data/train.csv")
+test_set = read_csv("../data/test.csv")
+
+
+# eksploracja -------------------------------------------------------------
+dataset = bind_rows(train_set, test_set)
+dataset %>%
+  group_by(Pclass) %>%
+  tally() %>%
+  ungroup() %>%
+  mutate(percent = n/sum(n)) %>%
+  filter(is.na(Pclass))
+dataset %>%
+  group_by(Age) %>%
+  tally() %>%
+  ungroup() %>%
+  mutate(percent = n/sum(n)) %>%
+  filter(is.na(Age))
+
+toplot = dataset %>%
+  group_by(Sex, Survived) %>%
+  tally() %>%
+  ungroup() %>%
+  summarise(n)
+ggplot(data = na.omit(toplot), 
+       aes(x = Sex,
+           y = n,
+           fill = Survived)) +
+  geom_bar(stat = "identity", position = "fill")
+
+# feature engineering -----------------------------------------------------
+
+
+# druga faza eksploracji --------------------------------------------------
+
+
+# tranowanie i testowanie -------------------------------------------------
+
+
